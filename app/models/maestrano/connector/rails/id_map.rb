9b778f20d9823e22e19af8e3d8870582c24e1f84
Pierre Berard <pierre.berard@maestrano.com>
2015-12-04 19:19:04 +0100

    Removes before_save on idmap: should not be needed

--- app/models/maestrano/connector/rails/id_map.rb
+++ app/models/maestrano/connector/rails/id_map.rb
@@ -1,11 +1,4 @@
 module Maestrano::Connector::Rails
   class IdMap < ActiveRecord::Base
-    before_save: :normalize_entity_names
-
-    private
-      def normalize_entity_names
-        self.connec_entity = self.connec_entity.downcase.singularize
-        self.external_entity = self.external_entity.downcase.singularize
-      end
   end
 end
