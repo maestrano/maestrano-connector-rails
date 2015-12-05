7749d0669323a66133f1b9c63c2bcf16221dd89f
Pierre Berard <pierre.berard@maestrano.com>
2015-12-05 15:59:56 +0100

    Removes unused method

--- app/models/maestrano/connector/rails/sub_complex_entity_base.rb
+++ app/models/maestrano/connector/rails/sub_complex_entity_base.rb
@@ -38,22 +38,5 @@ module Maestrano::Connector::Rails
         klass.set_organization(organization_id)
       end
     end
-
-    def create_idmap(entity, organization)
-      if self.external?
-        Maestrano::Connector::Rails::IdMap.create(
-          external_id: self.get_id_from_external_entity_hash(entity),
-          external_entity: self.external_entity_name.downcase,
-          organization_id: organization.id
-        )
-      else
-        Maestrano::Connector::Rails::IdMap.create(
-          connec_id: entity['id'],
-          connec_entity: self.connec_entity_name.downcase,
-          organization_id: organization.id
-        )
-      end
-    end
-
   end
 end
