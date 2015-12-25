b4c38b9fa874445c450f22637f4fc31b5acdff10
Pierre Berard <pierre.berard@maestrano.com>
2015-12-25 16:32:50 +0100

    Uses hard coded routes instead of maestrano_routes helper :(

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Maestrano::Connector::Rails::Engine.routes.draw do
-  maestrano_routes
+  # maestrano_routes
+
+  #--------------------------------------------------------------------
+  # Maintainability issue
+  # Should use maestrano_routes but it creates a scoping error with the metadata routes
+  get '/maestrano/metadata', to: '/maestrano/rails/metadata#index'
+  get '/maestrano/metadata/:tenant', to: '/maestrano/rails/metadata#index', as: 'tenant'
+
+  namespace :maestrano do
+    namespace :auth do
+      resources :saml, only:[] do
+        get 'init', on: :collection
+        get 'init/:tenant', on: :collection, to: 'saml#init', as: 'tenant'
+        post 'consume', on: :collection
+      end
+    end
+
+    namespace :account do
+      resources :groups, only: [:destroy] do
+        resources :users, only: [:destroy], controller: 'group_users'
+      end
+    end
+  end
+  #--------------------------------------------------------------------
 
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 end
