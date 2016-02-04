57f56ef1a9c49785afcdf0958430f3f6a6d6fbff
Pierre Berard <pierre.berard@maestrano.com>
2016-02-04 22:38:13 +0100

    Renames migration and tables

---
+++ db/migrate/20151122162100_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :provider
+      t.string :uid
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :tenant
+
+      t.timestamps null: false
+    end
+
+    add_index :users, [:uid, :tenant], name: 'user_uid_index'
+  end
+end
