2b0e282d9ad7fc086d757ac2937ba09b3e529397
bruno <bruno.chauvet@maestrano.com>
2016-02-10 11:01:05 +1100

    scope signout route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Maestrano::Connector::Rails::Engine.routes.draw do
   maestrano_routes
 
   namespace :maestrano do
+  	match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
     post 'connec/notifications/:tenant' => 'connec#notifications'
   end
-
-  match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
 end
