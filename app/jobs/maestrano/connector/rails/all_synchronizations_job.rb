08aad1f53a539b878bd8961c21fc341d901ce9b2
bruno <bruno.chauvet@maestrano.com>
2016-02-11 18:16:27 +1100

    support optional parameters for Sync Job

--- app/jobs/maestrano/connector/rails/all_synchronizations_job.rb
+++ app/jobs/maestrano/connector/rails/all_synchronizations_job.rb
@@ -3,7 +3,7 @@ module Maestrano::Connector::Rails
     queue_as :default
 
     # Trigger synchronization of all active organizations
-    def perform
+    def perform(name=nil, count=nil)
       Maestrano::Connector::Rails::Organization.where("oauth_provider IS NOT NULL AND oauth_token IS NOT NULL").each do |o|
         next unless [true, 1].include?(o.sync_enabled)
         Maestrano::Connector::Rails::SynchronizationJob.perform_later(o, {})
