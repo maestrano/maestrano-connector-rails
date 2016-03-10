0a3751e118e96d26f000a79fb9543e2012598e41
Pierre Berard <pierre.berard@maestrano.com>
2016-03-10 14:58:37 +0000

    Decorates connector_logger

--- app/models/maestrano/connector/rails/connector_logger.rb
+++ app/models/maestrano/connector/rails/connector_logger.rb
@@ -1,9 +1,5 @@
 module Maestrano::Connector::Rails
   class ConnectorLogger
-
-    def self.log(level, organization, msg)
-      Rails.logger.method(level).call("org: #{organization.uid} (#{organization.tenant}). Msg: #{msg}")
-    end
-
+    include Maestrano::Connector::Rails::Concerns::ConnectorLogger
   end
 end
