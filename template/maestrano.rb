f9ab35d8b54e7dcde154ae953ca87e9e99aa3033
Pierre Berard <pierre.berard@maestrano.com>
2016-05-24 12:14:38 +0100

    Cleans up template for settings

--- template/maestrano.rb
+++ template/maestrano.rb
@@ -24,6 +24,7 @@
     # on http://api-sandbox.maestrano.io
     #
     config.api.host = Settings[tenant][:api_host]
+    config.connec.host = Settings[tenant][:connec_host]
     config.api.id = ENV[Settings[tenant][:api_id]]
     config.api.key = ENV[Settings[tenant][:api_key]]
 
@@ -130,9 +131,6 @@
 
     config.webhook.connec.notifications_path = Settings[tenant][:webhook][:connec][:notifications_path]
     config.webhook.connec.external_ids = true
-
-    config.api.host = Settings[tenant][:api_host]
-    config.connec.host = Settings[tenant][:connec_host]
     #
     # == Subscriptions
     # This is the list of entities (organizations,people,invoices etc.) for which you want to be
