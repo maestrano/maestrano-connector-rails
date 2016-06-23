30468f2f1bdede62729c7294eb5aa0934f28e675
Pierre Berard <pierre.berard@maestrano.com>
2016-06-23 14:49:43 +0100

    Adds endpoints for mno-hub

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Maestrano::Connector::Rails::Engine.routes.draw do
   namespace :maestrano do
   	match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
     post 'connec/notifications/:tenant' => 'connec#notifications'
+
+    resources :synchronizations, only: [:show, :create, :destroy]
   end
 end
