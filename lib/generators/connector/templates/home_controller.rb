b06315a472f7d91cc334f5c050891cac3bf2e95e
Marco Bagnasco <admin@hecticmojo.com>
2016-08-17 11:46:41 +0100

    Changes home_controller and raise ABCmetric for generated file

--- lib/generators/connector/templates/home_controller.rb
+++ lib/generators/connector/templates/home_controller.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 class HomeController < ApplicationController
-  before_action :organization_to_update, only: [:update]
-
   def index
     @organization = current_organization
     @displayable_synchronized_entities = @organization.displayable_synchronized_entities if @organization
   end
 
   def update
-    return redirect_to(:back) unless @updating_organization && is_admin?(current_user, @updating_organization)
-    old_sync_state = @updating_organization.sync_enabled
-    @updating_organization.synchronized_entities.keys.each do |entity|
-      @updating_organization.synchronized_entities[entity] = params[entity.to_s].present?
+    organization = Maestrano::Connector::Rails::Organization.find_by_id(params[:id])
+
+    return redirect_to(:back) unless organization && is_admin?(current_user, organization)
+    old_sync_state = organization.sync_enabled
+    organization.synchronized_entities.keys.each do |entity|
+      organization.synchronized_entities[entity] = params[entity.to_s].present?
     end
-    @updating_organization.sync_enabled = @updating_organization.synchronized_entities.values.any?
-    @updating_organization.check_historical_data(params['historical-data'].present?)
+    organization.sync_enabled = organization.synchronized_entities.values.any?
+    organization.check_historical_data(params['historical-data'].present?)
 
-    unable_to_start = start_synchronization(old_sync_state, @updating_organization)
+    start_synchronization(old_sync_state, organization) unless !old_sync_state && organization.sync_enabled
 
-    redirect_to(:back) unless unable_to_start
+    redirect_to(:back)
   end
 
   def synchronize
@@ -35,13 +35,7 @@ class HomeController < ApplicationController
   private
 
     def start_synchronization(old_sync_state, organization)
-      return redirect_to(:back), true unless !old_sync_state && organization.sync_enabled
       Maestrano::Connector::Rails::SynchronizationJob.perform_later(organization, {})
       flash[:info] = 'Congrats, you\'re all set up! Your data are now being synced'
-      false
-    end
-
-    def organization_to_update
-      @updating_organization = Maestrano::Connector::Rails::Organization.find_by_id(params[:id])
     end
 end
