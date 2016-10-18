bafdd8b2a42d49338aa4303d9655f15d21104931
bruno <bruno.chauvet@maestrano.com>
2016-10-18 23:48:36 +1100

    update templates to match 2.0.0 recommendations

--- template/routes.rb
+++ template/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
   mount Maestrano::Connector::Rails::Engine, at: '/'
-  mount Sidekiq::Web => '/sidekiq'
 
+  # Default Connector pages
   root 'home#index'
   get 'home/index' => 'home#index'
   get 'home/redirect_to_external' => 'home#redirect_to_external'
   get 'home/index' => 'home#index'
   put 'home/update' => 'home#update'
   post 'home/synchronize' => 'home#synchronize'
-
   get 'synchronizations/index' => 'synchronizations#index'
   get 'shared_entities/index' => 'shared_entities#index'
+
+  # OAuth workflow pages
+  match 'auth/:provider/request', to: 'oauth#create_omniauth', via: [:get, :post]
+  match 'signout_omniauth', to: 'oauth#destroy_omniauth', as: 'signout_omniauth', via: [:get, :post]
+  post 'auth/auth', to: 'auth#auth'
+
+  # Sidekiq Admin
+  require 'sidekiq/web'
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    username == ENV['SIDEKIQ_USERNAME'] && password == ENV['SIDEKIQ_PASSWORD']
+  end
+  mount Sidekiq::Web => '/sidekiq'
 end
