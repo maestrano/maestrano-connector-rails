c28343c89914283914f95f63c87547c3ad2fbfb6
bruno <bruno.chauvet@maestrano.com>
2016-12-28 22:38:11 +1100

    add commit hash and branch to version endpoint

--- app/controllers/version_controller.rb
+++ app/controllers/version_controller.rb
@@ -1,9 +1,12 @@
 class VersionController < ApplicationController
   def index
     framework_version = Gem.loaded_specs['maestrano-connector-rails'].version.version
+    branch = ENV['GIT_BRANCH']
+    commit = ENV['GIT_COMMIT_ID']
+
     respond_to do |format|
-      format.html { render text: "framework_version=#{framework_version}\n" }
-      format.json { render json: {framework_version: framework_version, env: Rails.env, ruby_version: RUBY_VERSION, ruby_engine: RUBY_ENGINE} }
+      format.html { render text: "framework_version=#{framework_version}\nci_branch=#{branch}\nci_commit=#{commit}\nenv=#{Rails}\nnv, ruby_version=#{RUBY_VERSION}\nruby_engine=#{RUBY_ENGINE}\n" }
+      format.json { render json: {framework_version: framework_version, ci_branch: branch, ci_commit: commit, env: Rails.env, ruby_version: RUBY_VERSION, ruby_engine: RUBY_ENGINE} }
     end
   end
 end
--- spec/controllers/version_controller_spec.rb
+++ spec/controllers/version_controller_spec.rb
@@ -11,7 +11,7 @@ describe VersionController, type: :controller do
 
     it 'returns a version hash' do
       subject
-      expect(JSON.parse(response.body)).to eql('framework_version'=>'1.2', 'env' => 'test', 'ruby_version' => RUBY_VERSION, 'ruby_engine' => RUBY_ENGINE)
+      expect(JSON.parse(response.body)).to eql('framework_version'=>'1.2', 'ci_branch' => nil, 'ci_commit' => nil, 'env' => 'test', 'ruby_version' => RUBY_VERSION, 'ruby_engine' => RUBY_ENGINE)
     end
   end
 end
