974ee48c4c03aea8a678575a3b173c1beffd906e
bruno <bruno.chauvet@maestrano.com>
2017-02-02 18:05:55 +1100

    fix migration

--- db/migrate/20170202033323_update_organization_metadata.rb
+++ db/migrate/20170202033323_update_organization_metadata.rb
@@ -1,15 +1,18 @@
 class UpdateOrganizationMetadata < ActiveRecord::Migration
   def change
-  	add_column :organizations, :push_disabled, :boolean
-  	add_column :organizations, :pull_disabled, :boolean
-  	# Migration to update the way we handle synchronized_entities for data sharing.
-	  # Before : synchronized_entities = {company: true}
-	  # After: synchronized_entities = {company: {can_push_to_connec: true, can_push_to_external: true}}
+    add_column :organizations, :push_disabled, :boolean
+    add_column :organizations, :pull_disabled, :boolean
+    change_column :organizations, :synchronized_entities, :text
 
-	  #We also add metadata from MnoHub
+    # Migration to update the way we handle synchronized_entities for data sharing.
+    # Before : synchronized_entities = {company: true}
+    # After: synchronized_entities = {company: {can_push_to_connec: true, can_push_to_external: true}}
+
+    #We also add metadata from MnoHub
     Maestrano::Connector::Rails::Organization.all.each do |o|
       o.set_instance_metadata
       o.reset_synchronized_entities
+      o.enable_historical_data(true) if o.push_disabled
     end
   end
 end
