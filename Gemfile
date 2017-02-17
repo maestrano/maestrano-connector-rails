bf93f49ccf94b0ae07fe5c1a118f138726e9bbe5
marcocode <admin@hecticmojo.com>
2017-02-17 16:14:37 +0000

    Refactor template

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'http://rubygems.org'
 
 gemspec
-# Add dependencies to develop your gem here.
 
+# Add dependencies to develop your gem here.
 group :development do
   gem 'activerecord-jdbcsqlite3-adapter', platforms: :jruby
   gem 'sqlite3', platforms: :ruby
--- template/maestrano_connector_template.rb
+++ template/maestrano_connector_template.rb
@@ -1,3 +1,96 @@
+# Ensures a compatible version of Rails is used
+RAILS_SUPPORTED = ['>= 4.2', '< 5.0']
+
+def apply_template!
+  no?(RUBY_VERSION)
+
+  check_compatible_rails_version
+  ensure_valid_options
+
+  #
+  # Rebuild the Gemfile from scratch
+  #
+  template 'files/Gemfile.tt', 'Gemfile', force: true
+
+  remove_file '.gitignore'
+  copy_file 'files/gitignore', '.gitignore'
+  copy_file 'files/rubocop.yml', '.rubocop.yml'
+
+  #
+  # Cleanup
+  #
+  after_bundle do
+    remove_dir 'app/mailers'
+    remove_dir 'test'
+    remove_file 'app/views/layouts/application.html.erb'
+    remove_file 'app/assets/stylesheets/application.css'
+    remove_file 'config/routes.rb'
+    copy_file 'files/spec_helper.rb', 'spec/spec_helper.rb'
+    copy_file 'files/routes.rb', 'config/routes.rb'
+
+    # Procfile and uat
+    copy_file 'files/Procfile', 'Procfile'
+    run 'cp config/environments/production.rb config/environments/uat.rb'
+    remove_file 'config/database.yml'
+    copy_file 'files/database.yml', 'config/database.yml'
+    run 'echo \'uat:
+    secret_key_base: <%= ENV["SECRET_KEY_BASE"] %>\' >> config/secrets.yml'
+
+    # Sidekiq
+    copy_file 'files/sidekiq.yml', 'config/sidekiq.yml'
+    copy_file 'files/sidekiq.rb', 'config/initializers/sidekiq.rb'
+
+    # Settings
+    run 'bundle exec rails g config:install'
+    remove_dir 'config/settings'
+    remove_file 'config/settings.yml'
+    run 'mkdir config/settings'
+    %w(development production test uat).each do |file|
+      copy_file "settings/#{file}.yml", "config/settings/#{file}.yml"
+    end
+    copy_file 'settings/settings.yml', 'config/settings.yml'
+
+    copy_file 'files/application-sample.yml', 'config/application-sample.yml'
+    # TODO not needed imho
+    # copy_file 'application-sample.yml', 'config/application.yml', force: true
+
+    application do
+      <<-RUBY
+        config.generators do |g|
+          g.test_framework :rspec, fixture: false
+          g.view_specs false
+          g.helper_specs false
+        end
+      RUBY
+    end
+
+    run 'SKIP_CONFIGURATION=true bundle exec rails g connector:install'
+    run 'bundle exec figaro install'
+    run 'bundle exec rake railties:install:migrations'
+    run 'SKIP_CONFIGURATION=true bundle exec rake db:migrate'
+
+    run 'bundler binstubs puma --force'
+    run 'bundler binstubs sidekiq --force'
+    run 'bundler binstubs rake --force'
+
+    remove_file 'config/initializers/maestrano.rb'
+    copy_file 'files/maestrano.rb', 'config/initializers/maestrano.rb'
+
+    # Init repo and commit
+    git :init
+    git add: '.'
+    git commit: "-a -m 'Initial commit'"
+  end
+end
+
+#
+# Add this template directory to source_paths so that Thor actions like
+# copy_file and template resolve against our source files. If this file was
+# invoked remotely via HTTP, that means the files are not present locally.
+# In that case, use `git clone` to download them to a local temporary dir.
+# Thanks @mattbrictson!
+#
+
 def current_directory
   @current_directory ||=
     if __FILE__ =~ %r{\Ahttps?://}
@@ -21,115 +114,33 @@ def source_paths
   Array(super) + [current_directory]
 end
 
-#
-# Rebuild the Gemfile from scratch
-#
-remove_file 'Gemfile'
-run 'touch Gemfile'
+# ==================================================
 
-add_source 'https://rubygems.org'
-if defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
-  run 'echo "ruby \'2.3.1\', :engine => \'jruby\', :engine_version => \'9.1.5.0\'" | cat - Gemfile > temp && mv temp Gemfile'
+# Ensure we're using a compatible Rails version
+def check_compatible_rails_version
+  requirement = Gem::Requirement.new(RAILS_SUPPORTED)
+  rails_version = Gem::Version.new(Rails::VERSION::STRING)
+  return if requirement.satisfied_by?(rails_version)
+
+  prompt = "This template requires Rails #{RAILS_SUPPORTED}. "\
+           "You are using #{rails_version}. Continue anyway?"
+  exit 1 if no?(prompt)
 end
 
-gem 'rails', '~> 4.2'
-gem 'turbolinks', '~> 2.5'
-gem 'jquery-rails'
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :jruby]
-gem 'uglifier', '>= 1.3.0'
-
-gem 'puma', require: false
-gem 'sinatra', require: false
-gem 'sidekiq'
-gem 'sidekiq-cron'
-
-gem 'maestrano-connector-rails'
-
-gem_group :production, :uat do
-  gem 'activerecord-jdbcmysql-adapter', platforms: :jruby
-  gem 'mysql2', platforms: :ruby
-  gem 'rails_12factor'
-end
-
-gem_group :test, :development do
-  gem 'activerecord-jdbcsqlite3-adapter', platforms: :jruby
-  gem 'sqlite3', platforms: :ruby
-  gem 'rubocop'
-end
-
-gem_group :test do
-  gem 'simplecov'
-  gem 'rspec-rails'
-  gem 'factory_girl_rails'
-  gem 'shoulda-matchers'
-  gem 'timecop'
-end
-
-remove_file '.gitignore'
-copy_file 'gitignore', '.gitignore'
-copy_file 'rubocop.yml', '.rubocop.yml'
-
-#
-# Cleanup
-#
-after_bundle do
-  remove_dir 'app/mailers'
-  remove_dir 'test'
-  remove_file 'app/views/layouts/application.html.erb'
-  remove_file 'app/assets/stylesheets/application.css'
-  remove_file 'config/routes.rb'
-  copy_file 'spec_helper.rb', 'spec/spec_helper.rb'
-  copy_file 'routes.rb', 'config/routes.rb'
-
-  # Heroku and uat
-  copy_file 'Procfile', 'Procfile'
-  run 'cp config/environments/production.rb config/environments/uat.rb'
-  remove_file 'config/database.yml'
-  copy_file 'database.yml', 'config/database.yml'
-  run 'echo \'uat:
-  secret_key_base: <%= ENV["SECRET_KEY_BASE"] %>\' >> config/secrets.yml'
-
-  # Sidekiq
-  copy_file 'sidekiq.yml', 'config/sidekiq.yml'
-  copy_file 'sidekiq.rb', 'config/initializers/sidekiq.rb'
-
-  # Settings
-  run 'bundle exec rails g config:install'
-  remove_dir 'config/settings'
-  remove_file 'config/settings.yml'
-  run 'mkdir config/settings'
-  %w(development production test uat).each do |file|
-    copy_file "settings/#{file}.yml", "config/settings/#{file}.yml"
+# Exit if the user has used invalid generator options.
+def ensure_valid_options
+  valid_options = {
+    skip_gemfile: false,
+    skip_bundle: false,
+    skip_git: false
+  }
+  valid_options.each do |key, expected|
+    next unless options.key?(key)
+    actual = options[key]
+    unless actual == expected
+      fail Rails::Generators::Error, "Unsupported option: #{key}=#{actual}"
+    end
   end
-  copy_file 'settings/settings.yml', 'config/settings.yml'
-
-  copy_file 'application-sample.yml', 'config/application-sample.yml'
-  copy_file 'application-sample.yml', 'config/application.yml', force: true
-
-  application do
-    <<-RUBY
-      config.generators do |g|
-        g.test_framework :rspec, fixture: false
-        g.view_specs false
-        g.helper_specs false
-      end
-    RUBY
-  end
-
-  run 'bundle exec rails g connector:install'
-  run 'bundle exec figaro install'
-  run 'bundle exec rake railties:install:migrations'
-  run 'bundle exec rake db:migrate'
-
-  run 'bundler binstubs puma --force'
-  run 'bundler binstubs sidekiq --force'
-  run 'bundler binstubs rake --force'
-
-  remove_file 'config/initializers/maestrano.rb'
-  copy_file 'maestrano.rb', 'config/initializers/maestrano.rb'
-
-  # Init repo and commit
-  git :init
-  git add: '.'
-  git commit: "-a -m 'Initial commit'"
 end
+
+apply_template!
