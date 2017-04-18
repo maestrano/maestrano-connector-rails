7afa498607671478561b539cfc2a0a2f2d912c28
marcocode <admin@hecticmojo.com>
2017-04-18 17:01:34 +0100

    Fix rubocop offenses

--- template/files/sidekiq.rb
+++ template/files/sidekiq.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'sidekiq/web'
 
 Sidekiq::Cron::Job.create(name: 'AllSynchronizationsJob runs every hour', cron: '0 * * * *', class: 'Maestrano::Connector::Rails::AllSynchronizationsJob')
