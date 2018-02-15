58f10c1051e471c0e2038a76904c6bb042a9a525
Adam Abdelaziz <adam.abdelaziz1591@gmail.com>
2018-02-15 16:22:52 -0500

    update routes file to use namespacing for home routes

--- template/files/routes.rb
+++ template/files/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
 
   # Default Connector pages
   root 'home#index'
-  get 'home/index' => 'home#index'
-  get 'home/redirect_to_external' => 'home#redirect_to_external'
-  get 'home/index' => 'home#index'
-  put 'home/update' => 'home#update'
-  post 'home/synchronize' => 'home#synchronize'
+
+  namespace 'home' do
+    get 'index' => 'home#index'
+    get 'redirect_to_external' => 'home#redirect_to_external'
+    put 'update' => 'home#update'
+    post 'synchronize' => 'home#synchronize'
+  end
+
   get 'synchronizations/index' => 'synchronizations#index'
   get 'shared_entities/index' => 'shared_entities#index'
 
