af7966d2a61e035e8066a135197e2f7cddd5fb07
Bruno Chauvet <bruno.chauvet@maestrano.com>
2018-03-12 14:14:40 +1100

    error handling when connector cannot load developer platform configuration

--- template/files/maestrano.rb
+++ template/files/maestrano.rb
@@ -1,7 +1,11 @@
 unless ENV['SKIP_CONFIGURATION']
-  if Rails.env.test?
-    Maestrano.configure { |config| config.environment = 'production' }
-  else
-    Maestrano.auto_configure
+  begin
+    if Rails.env.test?
+      Maestrano.configure { |config| config.environment = 'production' }
+    else
+      Maestrano.auto_configure
+    end
+  rescue => e
+    puts "Cannot load configuration #{e.message}"
   end
 end
