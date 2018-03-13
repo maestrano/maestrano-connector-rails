c5dd2d1cc2b37315602f9c158aa146c2b9a07b6a
Bruno Chauvet <bruno.chauvet@maestrano.com>
2018-03-13 11:26:23 +1100

    fix typo and time based attack

--- template/files/routes.rb
+++ template/files/routes.rb
@@ -20,7 +20,8 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   require 'sidekiq/cron/web'
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-    username == ENV['SIDEKIQ_USERNAME'] && password == ENV['SIDEKIQ_PASSWORD']
+    Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
+      Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
   end
   mount Sidekiq::Web => '/sidekiq'
 end
--- template/files/sidekiq.rb
+++ template/files/sidekiq.rb
@@ -2,5 +2,5 @@
 
 # Schedule cron jobs at a random minute to avoid crowding of all connectors
 minute = rand(60)
-Sidekiq::Cron::Job.create(name: 'all_syncrhonizations_job', cron: "#{minute} * * * *", class: 'Maestrano::Connector::Rails::AllSynchronizationsJob')
+Sidekiq::Cron::Job.create(name: 'all_synchronizations_job', cron: "#{minute} * * * *", class: 'Maestrano::Connector::Rails::AllSynchronizationsJob')
 Sidekiq::Cron::Job.create(name: 'update_configuration_job', cron: "#{minute} * * * *", class: 'Maestrano::Connector::Rails::UpdateConfigurationJob')
