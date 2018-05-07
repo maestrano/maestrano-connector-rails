745a013c0e8c108af6bfee22ea951c91307f12b9
Bruno Chauvet <bruno.chauvet@maestrano.com>
2018-05-07 10:23:34 +1000

    add guarding when no active organizations

--- app/jobs/maestrano/connector/rails/all_synchronizations_job.rb
+++ app/jobs/maestrano/connector/rails/all_synchronizations_job.rb
@@ -8,6 +8,8 @@ module Maestrano::Connector::Rails
                              .where.not(oauth_provider: nil, encrypted_oauth_token: nil)
                              .select { |o| [true, 1].include?(o.sync_enabled) }
 
+      return true if active_organizations.count.zero?
+
       time_span_seconds = (3600 / active_organizations.count).to_i
       active_organizations.each_with_index do |organization, i|
         Maestrano::Connector::Rails::SynchronizationJob.set(wait: time_span_seconds * i).perform_later(organization.id, {})
--- spec/jobs/all_synchronizations_job_spec.rb
+++ spec/jobs/all_synchronizations_job_spec.rb
@@ -1,15 +1,23 @@
 require 'spec_helper'
 
 describe Maestrano::Connector::Rails::AllSynchronizationsJob do
-  let(:organization_not_linked) { create(:organization, oauth_provider: 'salesforce', oauth_token: nil, sync_enabled: true) }
-  let(:organization_not_active) { create(:organization, oauth_provider: 'salesforce', oauth_token: '123', sync_enabled: 0) }
-  let(:organization_to_process) { create(:organization, oauth_provider: 'salesforce', oauth_token: '123', sync_enabled: true) }
-
   subject { Maestrano::Connector::Rails::AllSynchronizationsJob.perform_now() }
 
   describe 'perform' do
-    it 'does not calls sync entity' do
-      expect { subject }.to enqueue_job(Maestrano::Connector::Rails::SynchronizationJob).with(organization_to_process.id, anything)
+    context 'without organizations' do
+      it 'does not calls sync entity' do
+        expect { subject }.not_to enqueue_job(Maestrano::Connector::Rails::SynchronizationJob)
+      end
+    end
+
+    context 'with active organizations' do
+      let!(:organization_not_linked) { create(:organization, oauth_provider: 'salesforce', oauth_token: nil, sync_enabled: true) }
+      let!(:organization_not_active) { create(:organization, oauth_provider: 'salesforce', oauth_token: '123', sync_enabled: 0) }
+      let!(:organization_to_process) { create(:organization, oauth_provider: 'salesforce', oauth_token: '123', sync_enabled: true) }
+
+      it 'syncs only active organizations' do
+        expect { subject }.to enqueue_job(Maestrano::Connector::Rails::SynchronizationJob).with(organization_to_process.id, anything)
+      end
     end
   end
 end
