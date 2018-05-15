7ea6097912611df0d56f6de344d6d91d9002ae62
Bruno Chauvet <bruno.chauvet@maestrano.com>
2018-05-15 10:05:52 +1000

    destroy all jobs before creating new ones

--- template/files/sidekiq.rb
+++ template/files/sidekiq.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# Ensure all jobs are removed before creating them
+Sidekiq::Cron::Job.destroy_all!
+
 # Schedule cron jobs at a random minute to avoid crowding of all connectors
 minute = rand(60)
 Sidekiq::Cron::Job.create(name: 'all_synchronizations_job', cron: "#{minute} * * * *", class: 'Maestrano::Connector::Rails::AllSynchronizationsJob')
