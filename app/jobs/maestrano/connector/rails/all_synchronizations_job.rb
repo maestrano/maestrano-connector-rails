9797d70c4298449031d7831646c954002db8831b
Olivier Brisse <ouranos@donarproject.org>
2018-10-22 16:57:42 +1100

    [APPINT-1205] Fix jobs spreading
    Previous code would clump everything as soon as > 3600 organizations.
    Relying on the law of large numbers to spread evenly as we scale.
    This allows us to do more efficient DB query as well.

--- app/jobs/maestrano/connector/rails/all_synchronizations_job.rb
+++ app/jobs/maestrano/connector/rails/all_synchronizations_job.rb
@@ -4,15 +4,12 @@ module Maestrano::Connector::Rails
 
     # Trigger synchronization of all active organizations
     def perform(name = nil, count = nil)
-      active_organizations = Maestrano::Connector::Rails::Organization
-                             .where.not(oauth_provider: nil, encrypted_oauth_token: nil)
-                             .select { |o| [true, 1].include?(o.sync_enabled) }
-
-      return true if active_organizations.count.zero?
-
-      time_span_seconds = (3600 / active_organizations.count).to_i
-      active_organizations.each_with_index do |organization, i|
-        Maestrano::Connector::Rails::SynchronizationJob.set(wait: time_span_seconds * i).perform_later(organization.id, {})
+      Maestrano::Connector::Rails::Organization
+        .where.not(oauth_provider: nil, encrypted_oauth_token: nil)
+        .where(sync_enabled: true)
+        .select(:id)
+        .find_each do |organization|
+        Maestrano::Connector::Rails::SynchronizationJob.set(wait: rand(3600)).perform_later(organization.id, {})
       end
     end
   end
